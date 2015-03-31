6a3af56163c016b748854b736ca0e31e13d82231
Natalia Castillejo <natalia.castillejoarias@yale.edu>
2015-03-31 01:57:21 -0400

    made shorter; comments

--- jsfile.js
+++ jsfile.js
@@ -16,27 +16,37 @@ function areAnagrams() {
 		return true;
 	};
 // Used the internet to review regular expressions.
+// I shouldn't be repeating myself
+
+	var result;
+	var image;
+
 	if(s1.length === 0 || s2.length === 0){
-	document.getElementById("result").innerHTML = "Hey! Remember to type in one word or phrase in each text field before submitting.";
-	document.getElementById("changeImage").src = "http://s24.postimg.org/6lajft16d/nosmile.jpg";
+	result = "Hey! Remember to type in one word or phrase in each text field before submitting.";
+	image = "http://s24.postimg.org/6lajft16d/nosmile.jpg";
 	} else if (s1 == s2) {
-	document.getElementById("result").innerHTML = "The words or phrases need to be different in order to be anagrams of each other. Try again.";
-	document.getElementById("changeImage").src = "http://s24.postimg.org/6lajft16d/nosmile.jpg";
+	result = "The words or phrases need to be different in order to be anagrams of each other. Try again.";
+	image = "http://s24.postimg.org/6lajft16d/nosmile.jpg";
 	}
 	else if (!/^[a-zA-Z ]+$/.test(s1) || !/^[a-zA-Z ]+$/.test(s2)){
-		document.getElementById("result").innerHTML = "Oops! Only alphabetic characters are allowed. Please try again.";
-		document.getElementById("changeImage").src = "http://s21.postimg.org/oscwtfh53/error.jpg";
+	result = "Oops! Only alphabetic characters are allowed. Please try again.";
+	image = "http://s21.postimg.org/oscwtfh53/error.jpg";
 	}
 	else if (isAnagram(s1,s2)){
-	document.getElementById("result").innerHTML = "Yay! They are anagrams of each other.";
-	document.getElementById("changeImage").src = "http://s11.postimg.org/e99xsslxf/areanagrams.jpg";
+	result = "Yay! They are anagrams of each other.";
+	image = "http://s11.postimg.org/e99xsslxf/areanagrams.jpg";
 	} else {
-	document.getElementById("result").innerHTML = "Oh no! They are not anagrams of each other."
-	document.getElementById("changeImage").src = "http://s3.postimg.org/rrpiegdgj/notanagram.jpg";
+	result = "Oh no! They are not anagrams of each other."
+	image = "http://s3.postimg.org/rrpiegdgj/notanagram.jpg";
 	};
-}
+	
+	document.getElementById("result").innerHTML = result;
+	document.getElementById("changeImage").src = image;
+	
+};
+//used internet to implement enter function
 document.onkeydown=function(){
     if(window.event.keyCode=='13'){
         areAnagrams();
     }
-}
+};
